7bd34055d366d5ed53362f1c1de74285a2aa8cb2
Jason B <lpfake@gmail.com>
2018-08-26 11:35:37 -0700

    Add files via upload
    Added more options for push stuff. Ability to change subject, always alert, and to use no push at all.

--- backupPFsense.ps1
+++ backupPFsense.ps1
@@ -6,9 +6,14 @@ $pw = "" #PFSense Password
 $pfaddress = "192.168.x.x" #PFSense address
 $retention = "-30" #how many days to keep old backup files (Must be negative value, -30 = files older than 30 days)
 $fileName = "pfsensebackup" #name of your log file (put into your $backupdir)
-$usepush = "n" #use pushover? (y or n)
-$pushoverapp = "c:\somepath\pushovercli.exe" #location of the pushovercli
 $minbackups = "5" #minimum number of backups to keep
+
+# Push options
+$usepush = "n" #use pushover? (y or n)
+$pushalways = "y" #send push on good backups
+$pushoverapp = "c:\somepath\pushovercli.exe" #location of the pushovercli
+$pushsubject = "pfSense Backup" #subject of the push message
+
 
 #Should not need to be modified
 $logFileName = $fileName + ".log"
@@ -17,32 +22,32 @@ $HowOld = (Get-Date).AddDays($retention);
 $command= $appdir + "pfSenseBackup.exe" + " -u " + $username + " -p " + $pw + " -s " + $pfaddress + " -o " + $backupdir
 
 function write-log {
-  param(
-  [parameter(Mandatory=$true)]
-  [string]$text,
-  [parameter(Mandatory=$true)]
-  [ValidateSet("WARNING","ERROR","INFO", "AppOutput")]
-  [string]$type
-  )
-  [string]$logMessage = [System.String]::Format("[$(Get-Date)] -"),$type, $text
-  Add-Content -Path $log -Value $logMessage
+	param(
+		[parameter(Mandatory=$true)]
+		[string]$text,
+		[parameter(Mandatory=$true)]
+		[ValidateSet("WARNING","ERROR","INFO", "AppOutput")]
+		[string]$type
+		)
+    [string]$logMessage = [System.String]::Format("[$(Get-Date)] -"),$type, $text
+	Add-Content -Path $log -Value $logMessage
 }
 
 function create_backup {
-  write-log -text "Executing the backup command..." -type INFO
-	$cmdOutput = Invoke-Expression $command | Out-String
-  write-log -text $cmdOutput -type AppOutput
-	if ($cmdOutput -like '*DONE*' )
-    {
-      write-log -text "Copy completed successfully." -type INFO
-      deloldbackups;
-    }
-  else 
-    {
-      write-log -text "ERROR running copy exe" -type ERROR
-      sendpush -message "PFSense auto backup failed!"
-      exit
-    }
+    write-log -text "Executing the backup command" -type INFO
+    $cmdOutput = Invoke-Expression $command | Out-String
+    write-log -text $cmdOutput -type AppOutput
+		if ($cmdOutput -like '*DONE*' )
+            {
+                write-log -text "Copy completed successfully." -type INFO
+                deloldbackups;
+            }
+       else {
+                write-log -text "ERROR running copy exe" -type ERROR
+                sendpush -message "PFSense auto backup failed!"	
+                exit
+            }
+		
 }
 
 function deloldbackups {
@@ -62,20 +67,26 @@ function deloldbackups {
 function alldone {
 	write-log -text "Backup completed successfully" -type INFO;
 	write-host "Backup complete."
-	exit
+	if ($pushalways = "y") {  
+		sendpush -message "PFSense auto backup completed successfully"
+        exit
+    }
+	else {
+		exit
+	}
 }
 
 function sendpush($message) {
-	if ($usepush = "y") {
-		write-log -text "Push notification sent" -type INFO;
-		$message = $message.ForEach({ "PFSense BACKUP - " + $message})
+    if ($usepush = "y") {  
+        write-log -text "Push notification sent" -type INFO;
+		$message = $message.ForEach({ "$pushsubject - " + $message})
 		& $pushoverapp message="$message";
-		exit
-	}
-  	else {
-  		write-log -text "Push not used or variable incorrect"
-  		exit
-  	}
+        exit
+    }
+    else {
+        write-log -text "Push not used or variable incorrect"
+        exit
+        }
 }
 
 Add-Content -Path $log -Value "--------------------------------------------------------------------"
